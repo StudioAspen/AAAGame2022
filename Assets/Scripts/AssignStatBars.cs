f927a6bc630f7b2378e43270033e3a9e7f7cf55b
habinpark <83361825+habinpark@users.noreply.github.com>
2022-11-20 16:27:29 -0500

    Added slider value assignment
    Only for HP and MP sliders and it is only set up on the first player on the canvas

---
+++ Assets/Scripts/AssignStatBars.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AssignStatBars : MonoBehaviour
+{
+    public CombatUnit combatUnit;
+    public Slider HPSlider;
+    public Slider MPSlider;
+    public Slider CDSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MaxHP = combatUnit.currentStats.maxHP;
+        float CurrentHP = combatUnit.currentStats.HP;
+
+        float HPSliderValue = CurrentHP / MaxHP;
+
+        float MaxMP = combatUnit.currentStats.maxMP;
+        float CurrentMP = combatUnit.currentStats.MP;
+
+        float MPSliderValue = CurrentMP / MaxMP;
+
+        HPSlider.value = HPSliderValue;
+        MPSlider.value = MPSliderValue;
+
+
+
+
+
+    }
+
+}
