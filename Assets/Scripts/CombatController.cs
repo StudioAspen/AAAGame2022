e1c916f11cb1a1a10b71b17fdff877e2ced9a8a0
habinpark <83361825+habinpark@users.noreply.github.com>
2022-11-20 15:46:23 -0500

    scripts and stuff

---
+++ Assets/Scripts/CombatController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CombatController : MonoBehaviour
+{
+    public GameObject skillButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+                
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void IntializeCombat(List<CombatUnit> playerUnits, List<CombatUnit> enemyUnits)
+    {
+        GameObject currentSkillButton = Instantiate(skillButton);
+        currentSkillButton.GetComponentInChildren<TMP_Text>();
+        currentSkillButton.GetComponent<Button>();
+    }
+}
