6344274e1eb99dadf333e074626990bacbfb1d61
TheStress <lieu.nelson386@gmail.com>
2023-01-08 18:27:52 -0500

    organized files and added method run after dialogue

--- Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 using TMPro;
 
 public class DialogueManager : MonoBehaviour
@@ -12,18 +13,28 @@ public class DialogueManager : MonoBehaviour
 
     [SerializeField]
     public DialogueBox dialogueBox;
+
     
     public void StartDialogue(DialogueInteraction dialogueInteraction)
     {
         if (!dialogueBox.isActiveAndEnabled)
         {
-            dialogueBox.gameObject.SetActive(true); //activating dialogue box
+            dialogueBox.gameObject.SetActive(true); 
             dialogueBox.dialogueManager = this;
             StartCoroutine(RunDialogue(dialogueInteraction.dialogues));
         }
     }
 
-    private IEnumerator RunDialogue(Dialogue[] dialogues)
+    public void StartDialogue(DialogueInteraction dialogueInteraction, UnityEvent actionAfterDialogue)
+    {
+        if (!dialogueBox.isActiveAndEnabled)
+        {
+            dialogueBox.gameObject.SetActive(true);
+            dialogueBox.dialogueManager = this;
+            StartCoroutine(RunDialogue(dialogueInteraction.dialogues, actionAfterDialogue));
+        }
+    }
+    private IEnumerator RunDialogue(Dialogue[] dialogues, UnityEvent actionAfterDialogue = null)
     {
         Dialogue currentDialogue;
         for (int i = 0; i < dialogues.Length; i++)
@@ -42,5 +53,9 @@ public class DialogueManager : MonoBehaviour
             completedDialogue = false;
         }
         dialogueBox.gameObject.SetActive(false);
+        if (actionAfterDialogue != null)
+        {
+            actionAfterDialogue.Invoke();
+        }
     }
 }
