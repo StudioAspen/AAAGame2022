cb983ce3a2181d0cda9b74ff93a097e14d68ebbe
brpasiliao <brpasiliao@gmail.com>
2023-02-24 17:33:01 -0500

    dialogue character change

--- Assets/Scripts/DialogueSystem/CharacterPoses.cs
+++ Assets/Scripts/DialogueSystem/CharacterPoses.cs
@@ -2,11 +2,9 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/CharacterPoses", order = 1)]
 public class CharacterPoses : ScriptableObject
 {
-    //public string name;
+    public string name;
     public Image[] poses;
 }
--- Assets/Scripts/DialogueSystem/DialogueBox.cs
+++ Assets/Scripts/DialogueSystem/DialogueBox.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class DialogueBox : MonoBehaviour
@@ -10,12 +11,45 @@ public class DialogueBox : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI textComponents;
     [SerializeField]
+    private Image portrait1;
+    [SerializeField]
+    private Image portrait2;
+
     private float textSpeed;
-
-    public Dialogue currentDialogue;
-    
     private int index;
+    private Image currentPortrait;
+    private Image otherPortrait;
+    public Dialogue currentDialogue;
 
+    private Dictionary<string, int> poses;
+    private Dictionary<string, Sprite[]> characters;
+
+    [SerializeField]
+    private Sprite[] cynthiSprites = new Sprite[5];
+    [SerializeField]
+    private Sprite[] alonsoSprites = new Sprite[5];
+    [SerializeField]
+    private Sprite[] zinniaSprites = new Sprite[5];
+
+    void Awake() {
+        currentPortrait = portrait2;
+
+        poses = new Dictionary<string, int>() 
+        {
+            { "neutral", 0 },
+            { "happy", 1 },
+            { "sad", 2 },
+            { "angry", 3 },
+            { "shock", 4 },
+        };
+
+        characters = new Dictionary<string, Sprite[]>() 
+        {
+            { "Cynthi", cynthiSprites },
+            { "Alonso", alonsoSprites },
+            { "Zinnia (O.S.)", zinniaSprites },
+        };
+    }
 
     void Update()
     {
@@ -72,16 +106,31 @@ public class DialogueBox : MonoBehaviour
 
     void PlayAnimations()
     {
-        string poseKey;
-        if (currentDialogue.poses.TryGetValue(index, out poseKey))
+        if (currentPortrait == portrait1) 
         {
-            Debug.Log("Pose: " + poseKey); //Should be replaced with function
+            currentPortrait = portrait2;
+            otherPortrait = portrait1;
         }
-        string animationKey;
-        if (currentDialogue.animations.TryGetValue(index, out animationKey))
+        else 
         {
-            Debug.Log("Animation: " + animationKey); //Should be replaced with function
+            currentPortrait = portrait1;
+            otherPortrait = portrait2;
         }
+
+        currentPortrait.sprite = characters[currentDialogue.name][poses[currentDialogue.poses[index]]];
+        currentPortrait.color = new Color(1, 1, 1, 1);
+        otherPortrait.color = new Color(1, 1, 1, 0.5f);
+
+        // string poseKey;
+        // if (currentDialogue.poses.TryGetValue(index, out poseKey))
+        // {
+        //     Debug.Log("Pose: " + poseKey); //Should be replaced with function
+        // }
+        // string animationKey;
+        // if (currentDialogue.animations.TryGetValue(index, out animationKey))
+        // {
+        //     Debug.Log("Animation: " + animationKey); //Should be replaced with function
+        // }
     }
 
 }
