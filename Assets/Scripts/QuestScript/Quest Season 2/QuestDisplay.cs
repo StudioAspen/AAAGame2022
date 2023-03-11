d93816f45234aace3a28e3f575940ff7310d8c08
kyleyang2020 <kyleyang2020@gmail.com>
2023-03-11 00:43:48 -0500

    display works

--- Assets/Scripts/QuestScript/Quest Season 2/QuestDisplay.cs
+++ Assets/Scripts/QuestScript/Quest Season 2/QuestDisplay.cs
@@ -7,53 +7,26 @@ public class QuestDisplay : MonoBehaviour
 {
     public GameObject myPrefab;
     public Transform parent;
+    int current_quest = 0; //tracker to update quest list DISPLAY
 
     // Update is called once per frame
     void Update()
     {
         GameObject tempPrefab;
         int num_quest = FindObjectOfType<QuestManager>().all_quests.Count; //for current amount of quest in list
-        int current_quest = 0; //tracker to update quest list DISPLAY
 
-        //when a new quest has been added
-        if(!(num_quest > current_quest)) {
-            for (int i = 0; i < num_quest; i++)
-            {
-                tempPrefab = Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-                Text test;
-                tempPrefab.TryGetComponent<Text>(out test);
-                test.text = FindObjectOfType<QuestManager>().all_quests[current_quest].title;
-                tempPrefab.transform.SetParent(parent);
-                current_quest++;
-            }
-            Debug.Log(current_quest + " = quest displayed, " + num_quest + " = quests in list");
-        }
-
-        //when a quest has been completed and removed
-        /*
-        if (num_quest < current_quest)
+        if(!(num_quest == current_quest))
         {
-            for (int i = 0; i < num_quest; i++)
+            for (int i = parent.transform.childCount - 1; i >= 0; i--)
             {
-                tempPrefab = Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-                Text test;
-                tempPrefab.TryGetComponent<Text>(out test);
-                test.text = "quest name";
-                tempPrefab.transform.SetParent(parent);
-                current_quest = num_quest;
+                Destroy(parent.transform.GetChild(i).gameObject);
             }
-            enabled = false;
-            current_quest = num_quest;
-        }
-        */
-
-        void DisplayUpdate(List<Quest> list) {
             for (int i = 0; i < num_quest; i++)
             {
                 tempPrefab = Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
                 Text test;
                 tempPrefab.TryGetComponent<Text>(out test);
-                test.text = FindObjectOfType<QuestManager>().all_quests[current_quest].title;
+                test.text = FindObjectOfType<QuestManager>().all_quests[i].title;
                 tempPrefab.transform.SetParent(parent);
                 current_quest++;
             }
